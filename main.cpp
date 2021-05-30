a8392b32fb4f8593f2fb8329dae1be5888075758
Emanet <44415149+Emanet@users.noreply.github.com>
2021-05-30 23:35:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+
 using std::string;
 
 class Shape {
 public:
-    virtual double getArea()const = 0;
+    virtual double getArea() const = 0;
 
 
 protected:
@@ -13,67 +14,10 @@ protected:
     double *y;
     string color;
 };
-class Line :public Shape{
+
+class Line : public Shape {
 public:
-    Line(double x1,double y1, double x2, double y2){
-        x = (double*) malloc(sizeof (double)*2);
-        y = (double*) malloc(sizeof (double)*2);
-        if (x == NULL || y == NULL){
-            std::cout<<"Yeterli Bellek Tahsis Edilemedi\n";
-            exit(1);
-        }
-        x [0] = x1;
-        x [1] = x2;
-        y [0] = y1;
-        y [1] = y2;
-
-    }
-
-    Line() {}
-
-    ~Line(){
-        free(x);
-        free(y);
-    }
-
-    double getArea()const{
-        double xLength = +(x[0]-x[1]);
-        double yLength = +(y[0]-y[1]);
-        double area = sqrt(pow(xLength,2)+ pow(yLength,2));
-        return area;
-    };
-
-    string toString()const{
-        string var = std::to_string(x[0])+"  "+std::to_string(y[0])+"\n"+std::to_string(x[1])+"  "+std::to_string(y[1]);
-        return var;
-    }
-};
-class VerticalLine :public Line{
-public:
-    VerticalLine(double x1,double y1,double y2){
-        x = (double*) malloc(sizeof (double)*2);
-        y = (double*) malloc(sizeof (double)*2);
-        if (x == NULL || y == NULL){
-            std::cout<<"Yeterli Bellek Tahsis Edilemedi\n";
-            exit(1);
-        }
-        x [0] = x1;
-        x [1] = x1;
-        y [0] = y1;
-        y [1] = y2;
-
-    }
-    double getArea() const override{
-        return +(y[0]-y[1]);
-    }
-    string toString()const{
-     return std::to_string(x[0])+"  "+std::to_string(y[0])+"\n"+std::to_string(x[1])+"  "+std::to_string(y[1]);
-    }
-
-};
-class HorizontalLine :public Line {
-public:
-    HorizontalLine(double x1, double y1, double x2) {
+    Line(double x1, double y1, double x2, double y2) {
         x = (double *) malloc(sizeof(double) * 2);
         y = (double *) malloc(sizeof(double) * 2);
         if (x == NULL || y == NULL) {
@@ -82,6 +26,69 @@ public:
         }
         x[0] = x1;
         x[1] = x2;
+        y[0] = y1;
+        y[1] = y2;
+
+    }
+
+    Line() {}
+
+    ~Line() {
+        free(x);
+        free(y);
+    }
+
+    double getArea() const {
+        double xLength = +(x[0] - x[1]);
+        double yLength = +(y[0] - y[1]);
+        double area = sqrt(pow(xLength, 2) + pow(yLength, 2));
+        return area;
+    };
+
+    string toString() const {
+        string var = std::to_string(x[0]) + "  " + std::to_string(y[0]) + "\n" + std::to_string(x[1]) + "  " +
+                     std::to_string(y[1]);
+        return var;
+    }
+};
+
+class VerticalLine : public Line {
+public:
+    VerticalLine(double x1, double y1, double length) {
+        x = (double *) malloc(sizeof(double) * 2);
+        y = (double *) malloc(sizeof(double) * 2);
+        if (x == NULL || y == NULL) {
+            std::cout << "Yeterli Bellek Tahsis Edilemedi\n";
+            exit(1);
+        }
+        x[0] = x1;
+        x[1] = x1;
+        y[0] = y1;
+        y[1] = y1 + length;
+    }
+
+    double getArea() const override {
+        return +(y[0] - y[1]);
+    }
+
+    string toString() const {
+        return std::to_string(x[0]) + "  " + std::to_string(y[0]) + "\n" + std::to_string(x[1]) + "  " +
+               std::to_string(y[1]);
+    }
+
+};
+
+class HorizontalLine : public Line {
+public:
+    HorizontalLine(double x1, double y1, double length) {
+        x = (double *) malloc(sizeof(double) * 2);
+        y = (double *) malloc(sizeof(double) * 2);
+        if (x == NULL || y == NULL) {
+            std::cout << "Yeterli Bellek Tahsis Edilemedi\n";
+            exit(1);
+        }
+        x[0] = x1;
+        x[1] = x1 + length;
         y[0] = y1;
         y[1] = y1;
 
@@ -96,12 +103,41 @@ public:
                std::to_string(y[1]);
     }
 };
-int main (){
-    Line a = {10,24,17,48};
-    std::cout<<a.toString();
-    VerticalLine b = {5,10,20};
-    std::cout<<b.toString();
-    HorizontalLine c = {3,5,7};
-    std::cout<<c.toString();
 
+class Triangle : public Shape {
+public:
+    Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+        x = (double *) malloc(sizeof(double) * 3);
+        y = (double *) malloc(sizeof(double) * 3);
+        if (x == NULL || y == NULL) {
+            std::cout << "Yeterli Bellek Tahsis Edilemedi\n";
+            exit(1);
+        }
+        x[0] = x1;
+        x[1] = x2;
+        x[2] = x3;
+        y[0] = y1;
+        y[1] = y2;
+        y[2] = y3;
+    };
+
+    ~Triangle() {
+        free(x);
+        free(y);
+    };
+
+    double getArea() const {
+        double area = abs((x[1] - x[0])*(y[2] - y[0]) - (x[2] - x[0])*(y[1] - y[0]))/2.0;
+        return area;
+    };
+
+    string toString() const {
+        return std::to_string(x[0]) + "  " + std::to_string(y[0]) + "\n" + std::to_string(x[1]) + "  " +
+               std::to_string(y[1]) + std::to_string(x[2]) + "  " + std::to_string(y[2]);
+    }
+};
+
+int main() {
+    Triangle a = {0.0, 0.0, 0.0, 1.0, 1.0, 1.0};
+    std::cout << a.getArea();
 }
